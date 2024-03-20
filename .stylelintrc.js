00c5b4fbecfa8aaa099b30494c2da77b7989ea27
yaoc <yyl134934@163.com>
2024-03-20 21:35:51 +0800

    chore(stylelint):stylelint添加bem规则

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,20 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
-  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties', 'stylelint-prettier'],
+  plugins: [
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties',
+    'stylelint-prettier',
+    'stylelint-selector-bem-pattern',
+  ],
   rules: {
+    'plugin/selector-bem-pattern': {
+      componentName: '[A-Z]+',
+      componentSelectors: {
+        initial: '^\\.{componentName}(?:-[a-z]+)?$',
+        combined: '^\\.combined-{componentName}-[a-z]+$',
+      },
+      utilitySelectors: '^\\.util-[a-z]+$',
+    },
     'plugin/declaration-block-no-ignored-properties': true,
     'comment-empty-line-before': null,
     'declaration-empty-line-before': null,
@@ -10,6 +23,7 @@ module.exports = {
     'no-invalid-double-slash-comments': null,
     'rule-empty-line-before': 'always',
     'prettier/prettier': true,
+    'selector-class-pattern': null,
   },
-  ignoreFiles: ['node_modules/**/*', 'build/**/*'],
+  ignoreFiles: ['node_modules/**/*', 'dist/**/*'],
 };
