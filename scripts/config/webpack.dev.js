b971775bf20bdbae3ab6ac305c7a9dbe29e2954c
yaoc <yyl134934@163.com>
2024-03-21 12:24:46 +0800

    chore(hmp):调整热更新策略 - devtool:'eval-source-map'

--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const { SERVER_HOST, SERVER_PORT } = require('../constants');
@@ -6,7 +5,7 @@ const proxyConfigs = require('../../src/setProxy');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'eval-cheap-module-source-map',
+  devtool: 'eval-source-map',
   devServer: {
     host: SERVER_HOST, // 地址
     port: SERVER_PORT, // 端口
@@ -16,9 +15,8 @@ module.exports = merge(common, {
       logging: 'info', // 日志等级
     },
     compress: true, // 是否启用 gzip 压缩
-    open: true, // 打开默认浏览器
+    open: false, // 打开默认浏览器
     // overlay: { warnings: true, errors: true }, // 出现编译器错误或警告时，在浏览器中显示全屏覆盖
     // proxy: { ...proxyConfigs },//代理（处理跨域问题）
   },
-  // plugins: [new webpack.HotModuleReplacementPlugin()],
 });
