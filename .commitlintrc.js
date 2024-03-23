5780d803d41430736905ddc3d75915da14d3a743
yaoc <yyl134934@163.com>
2024-03-23 17:13:48 +0800

    chore(commitlint):更新commitlint相关命令

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-            2,
-            'always',
-            ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'anno'],
-        ],
-    },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'anno'],
+    ],
+    'subject-full-stop': [2, 'always'],
+    'subject-max-length': [2, 'always', 72],
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 72],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-line-length': [2, 'always', 72],
+  },
 };
